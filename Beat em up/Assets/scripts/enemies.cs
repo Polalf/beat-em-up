a09a68a3b3b7944d21ad4cba933a5a2ce23caabf
Polalf <103451330+Polalf@users.noreply.github.com>
2022-10-24 18:02:31 -0300

    aaaaa

--- Beat em up/Assets/scripts/enemies.cs
+++ Beat em up/Assets/scripts/enemies.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class enemies : MonoBehaviour
 {
-    //[Header("Life")]
-    //public int maxLife;
-    //public int currentLife;
-    //[Header("Move")]
-    //[Header("Attack")]
-    //void Start()
-    //{
-        
-    //}
+    [Header("Life")]
+    public int maxLife;
+    public int currentLife;
+    [Header("Move")]
+    [Header("Attack")]
+    public float range;
+    private float distpl;
+    public Transform player;
+    void Start()
+    {
 
-    
-    //void Update()
-    //{
-    //    if(currentLife <= 0)
-    //    {
-    //        destroy(this.gameObject);
-    //    }
+    }
 
-    //}
+
+    void Update()
+    {
+        distpl = Vector2.Distance(transform.position, player.position);
+        if(distpl<=range)
+        {
+
+        }
+        if (currentLife <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
 }
